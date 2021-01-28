ea2e13576083cdd8276830b886e92e120af64004
snihurenko <snigurenko.anastasiya@gmail.com>
2021-01-28 20:52:07 +0200

    new feat

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-var employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
-var nameCourse = 'Базовый React';
-var command = [];
-for (var i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].trim() != '') {
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+const nameCourse = 'Базовый React';
+const command = [];
+for (let i = 0; i < employers.length; i++) {
+	if (employers[i].length > 0 && employers[i].trim() !== '') {
 		command.push(employers[i]);
 	}
 }
-for (var i = 0; i < command.length; i++) {
+for (let i = 0; i < command.length; i++) {
 	command[i] = command[i].toLowerCase().trim();
 	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
 }
 
-var data = {
+const data = {
 	cash: [3, 8, 3],
 	react: ['JSX', 'components', 'props', 'state', 'hooks'],
 	add: ['styled-components', 'firebase']
 };
 
-function calcCash(own) {
-	own = own || 0;
-	var everyCash = Array.prototype.slice.call(arguments);
-	var total = own;
-	for (var i = 0; i < everyCash[1].length; i++) {
-		total += +everyCash[1][i];
+const calcCash = (own) => {
+	let total = 0;
+	for (let i = 0; i < own.length; i++) {
+		total += +own[i];
 	}
 	return total;
-}
+};
 
-var lesson = calcCash(null, data.cash);
+const lesson = calcCash(data.cash);
 
-function makeBusiness(director, teacher, allModule, gang, course) {
+const makeBusiness = (director, teacher, allModule, gang, course) => {
 	teacher = teacher || 'Максим';
-	var sumTech = data.react.concat(data.add, 'и другие');
-	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director + ', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' +
-		gang);
-	console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
-	console.log('Технологии которые мы изучим: ');
-	console.log.apply(null, sumTech);
+	const sumTech = data.react.concat(data.add, 'и другие');
+	console.log(`Стартуем новый курс: ${course}. Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
+Команда Академии: ${gang}`);
+	console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
+	console.log('Технологии которые мы изучим:');
+	console.log(...sumTech);
+};
 
-}
+makeBusiness(...['Артем', , lesson, command, nameCourse]);
 
-makeBusiness.apply(null, ['Артем', null, lesson, command, nameCourse]);
+
