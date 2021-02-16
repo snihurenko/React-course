9a20e2dc7b8d83db9eec689a3cbb375dbbd135c7
snihurenko <snigurenko.anastasiya@gmail.com>
2021-02-16 12:36:10 +0200

    add fetch to load menu from db

--- food-app/src/Components/Menu/Menu.js
+++ food-app/src/Components/Menu/Menu.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
-
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
     background-color: #ccc;
@@ -15,17 +14,28 @@ const SectionMenu = styled.section`
     padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-    <MenuStyled>
-        <Banner/>
-        <SectionMenu>
-            <h2>Бургеры</h2>
-            <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem}/>
-        </SectionMenu>
+export const Menu = ({ setOpenItem }) => {
+    const res = useFetch();
+    const dbMenu = res.response;
 
-        <SectionMenu>
-            <h2>Закуски / Напитки</h2>
-            <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem}/>
-        </SectionMenu>
-    </MenuStyled>
-);
+    return (
+        <MenuStyled>
+            <Banner/>
+            {res.response ? 
+            <>
+                <SectionMenu>
+                    <h2>Бургеры</h2>
+                    <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem}/>
+                </SectionMenu>
+
+                <SectionMenu>
+                    <h2>Закуски / Напитки</h2>
+                    <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem}/>
+                </SectionMenu>
+            </>
+            : res.error ? <div>Sorry, we will fix it</div> : <div>Loading...</div>
+            
+            }
+        </MenuStyled>
+    )
+};
